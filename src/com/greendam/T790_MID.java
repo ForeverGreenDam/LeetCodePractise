1a2624aafe8ca3fa9aaa2db91182b7da41e48627
ForeverGreenDam <2985601245@qq.com>
2025-05-05 13:56:30 +0800

    5.5T790，每日一题

---
+++ src/com/greendam/T790_MID.java
@@ -0,0 +1,19 @@
+package com.greendam;
+
+public class T790_MID {
+    static final int MOD = 1000000007;
+    public static void main(String[] args) {
+        System.out.println(numTilings(3));
+    }
+    public static int numTilings(int n) {
+        int[][] dp = new int[n + 1][4];
+        dp[0][3]=1;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][3];
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][3] = (((dp[i - 1][0] + dp[i - 1][1]) % MOD + dp[i - 1][2]) % MOD + dp[i - 1][3]) % MOD;
+        }
+        return dp[n][3];
+    }
+}
