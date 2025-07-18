c60dbcb908dfd8c9eeea9602a77bbc593fac6391
ForeverGreenDam <2985601245@qq.com>
2025-07-18 11:08:58 +0800

    7.18T104,HOT100-36

---
+++ src/com/greendam/T104_EZ.java
@@ -0,0 +1,12 @@
+package com.greendam;
+
+public class T104_EZ {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
