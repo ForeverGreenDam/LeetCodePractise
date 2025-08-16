829b0bc1210aa2e4cae0b742ec126b01c6817029
ForeverGreenDam <2985601245@qq.com>
2025-08-16 23:13:50 +0800

    8.16T72,HOT100-85

---
+++ src/com/greendam/T72_MID.java
@@ -0,0 +1,37 @@
+package com.greendam;
+
+public class T72_MID {
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+        T72_MID solution = new T72_MID();
+        System.out.println(solution.minDistance(word1, word2));
+    }
+    public int minDistance(String word1, String word2) {
+        int m=word1.length();
+        int n=word2.length();
+        if(m==0){
+            return n;
+        }
+        if(n==0){
+            return m;
+        }
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<n+1;i++){
+            dp[0][i]=i;
+        }
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(word1.charAt(i-1)!=word2.charAt(j-1)){
+                    dp[i][j]=Math.min(Math.min(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1])+1;
+                }else{
+                    dp[i][j]=Math.min(Math.min(dp[i][j-1]+1,dp[i-1][j]+1),dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
